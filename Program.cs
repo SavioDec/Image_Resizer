2d4bd37d1ccd817059113623b5dddd4d39a568a0
Sávio Decaro <savio.decaro@gmail.com>
2026-01-18 12:36:28 -0300

    add support for multiple image heights and input directory polling

--- Program.cs
+++ Program.cs
@@ -7,12 +7,35 @@ internal class Program {
     public static void Main(string[] args) {
         Console.WriteLine("Starting image resizer");
 
+        List<int> altura = PegarListaAltura();
 
-        Thread thread = new Thread(Redimensionar);
-        thread.Start();
+        Redimensionar(altura);
     }
 
-    private static void Redimensionar() {
+    private static List<int> PegarListaAltura() {
+        List<int> listaAlturas = new List<int>();
+        int quantidade = LerInteiro("How many Diferent Heights do you want? ");
+
+        for (int i = 1; i <= quantidade; i++) {
+            int altura = LerInteiro($"Enter height #{i} (in pixels)");
+            listaAlturas.Add(altura);
+        }
+
+        return listaAlturas;
+    }
+
+    private static int LerInteiro(string texto) {
+        while (true) {
+            Console.Write(texto);
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int valor) && valor > 0) return valor;
+
+            Console.WriteLine("Invalid input. Please enter a positive integer greater than zero.");
+        }
+    }
+
+    private static void Redimensionar(List<int> listaAltura) {
         #region Diretorios
 
         string diretorioEntrada = "Input_Files";
@@ -33,64 +56,56 @@ internal class Program {
 
         #endregion
 
+        while (Directory.GetFiles(diretorioEntrada).Length == 0) {
+            Console.Clear();
+            Console.WriteLine("Put the files in the Input_Files directory");
+
+            Thread.Sleep(2000);
+        }
+
         FileStream fileStream;
         FileInfo fileInfo;
 
         bool imagesSequence = true;
         while (imagesSequence) {
-            try {
-                //Programa olha para a pasta de entrada
-                // Se tiver arquivo, ele ira redimensionar
-                var arquivosEntrada = Directory.EnumerateFiles(diretorioEntrada);
-                if (arquivosEntrada.Count() == 0) {
-                    Console.WriteLine("Put the files in the Input_Files directory");
+            //Programa olha para a pasta de entrada
+            // Se tiver arquivo, ele ira redimensionar
+            var arquivosEntrada = Directory.EnumerateFiles(diretorioEntrada);
+
+            if (arquivosEntrada.Count() == 0) imagesSequence = false;
+
+
+            // ler o tamanho que ira redimensionar
+            foreach (var arquivo in arquivosEntrada) {
+                fileStream = new FileStream(arquivo, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+                fileInfo = new FileInfo(arquivo);
+
+
+                using (Image imagemOriginal = Image.Load(fileStream)) {
+                    foreach (var alturaAlvo in listaAltura) {
+                        string extensao = fileInfo.Extension;
+                        string nomeSemExtensao = Path.GetFileNameWithoutExtension(arquivo);
+                        string novoNome = $"{nomeSemExtensao}_H{alturaAlvo}_{DateTime.Now:dd_MM_yy_HHmmss}{extensao}";
+
+                        string caminho = Path.Combine(Environment.CurrentDirectory, diretorioRedimensionado, novoNome);
+
+                        //Redimensiona & Copia os arquivos redimensionados para a pasta de redimensionados
+                        Redimensionador(imagemOriginal.Clone(x => { }), alturaAlvo, caminho);
+                    }
                 }
 
-                Console.Write("Enter the desired new height: ");
-                var textoDigitado = Console.ReadLine();
-
-                //Trata Erros de input
-                if (!int.TryParse(textoDigitado, out int novaAltura)) throw new Exception($"The value '{textoDigitado}' needs to be an integer");
-                if (novaAltura <= 0) throw new Exception($"The value '{novaAltura}' cannot be negative");
-
-                if (arquivosEntrada.Count() == 0) {
-                    imagesSequence = false;
-                }
-
-                // ler o tamanho que ira redimensionar
-
-                foreach (var arquivo in arquivosEntrada) {
-                    fileStream = new FileStream(arquivo, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
-                    fileInfo = new FileInfo(arquivo);
-
-                    string extensao = fileInfo.Extension;
-                    string nomeSemExtensao = Path.GetFileNameWithoutExtension(arquivo);
-                    string novoNome = $"{nomeSemExtensao}_{DateTime.Now:dd_MM_yy_HHmmss}{extensao}";
-
-                    string caminho = Path.Combine(Environment.CurrentDirectory, diretorioRedimensionado, novoNome);
-
-                    //Redimensiona & Copia os arquivos redimensionados para a pasta de redimensionados
-                    Redimensionador(imagem: Image.Load(fileStream), novaAltura, caminho);
-
-                    //Fecha o arquivo
-                    fileStream.Close();
+                //Fecha o arquivo
+                fileStream.Close();
 
 
-                    //move arquivo de entrada para a pasta de finalizados
-                    string caminhoFinalizado =
-                        Path.Combine(Environment.CurrentDirectory, diretorioFinalizado, fileInfo.Name);
-                    fileInfo.MoveTo(caminhoFinalizado);
-                }
+                //move arquivo de entrada para a pasta de finalizados
+                string caminhoFinalizado = Path.Combine(Environment.CurrentDirectory, diretorioFinalizado, fileInfo.Name);
+                if(File.Exists(caminhoFinalizado)) File.Delete(caminhoFinalizado);
 
-                Thread.Sleep(new TimeSpan(0, 0, 1));
-
-
-                Console.WriteLine($"All files resized!");
+                fileInfo.MoveTo(caminhoFinalizado);
             }
-            catch (Exception e) {
-                Console.Clear();
-                Console.WriteLine($"Error: {e.Message}\nPlease enter a valid number");
-            }
+
+            if(imagesSequence) Console.WriteLine($"All files resized!");
         }
     }
 
